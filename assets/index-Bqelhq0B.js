04fa19782671f1a179832e6f9406ba7f4cd7043c
Yoav Toren <yoavtoren@gmail.com>
2026-07-10 02:58:17 +0300

    Updates

---
+++ assets/index-Bqelhq0B.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-mS_cV7X8.js","assets/index-DP2UCc_7.js","assets/index-DLay_BW-.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-DP2UCc_7.js";const _=p("App",{web:()=>r(()=>import("./web-mS_cV7X8.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
--- assets/web-DHkia1Bo.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-C4rxKzJu.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
