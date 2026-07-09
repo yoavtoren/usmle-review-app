4ad8f0eb50d09e61dd788788b7fdd01fb2a20cf5
Yoav Toren <yoavtoren@gmail.com>
2026-07-09 19:24:00 +0300

    Updates

--- assets/index-j19rv5_2.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DcZZ5BOj.js","assets/index-Dqy6pXYF.js","assets/index-BF6s7T9w.css"])))=>i.map(i=>d[i]);
-import{r as p,_ as r}from"./index-Dqy6pXYF.js";const _=p("App",{web:()=>r(()=>import("./web-DcZZ5BOj.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
