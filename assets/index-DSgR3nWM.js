ba23173172c23d960a1adbd41deec03db33ef8ec
Yoav Toren <yoavtoren@gmail.com>
2026-07-09 21:13:14 +0300

    Updates

--- assets/index-DSgR3nWM.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-zrt5IajR.js","assets/index-DP5mLYgP.js","assets/index-BI-AcWuJ.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-DP5mLYgP.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-zrt5IajR.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
