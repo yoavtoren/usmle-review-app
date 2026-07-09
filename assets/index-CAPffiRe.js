0bd37bb321569b04a186767badc7a872003f334c
Yoav Toren <yoavtoren@gmail.com>
2026-07-09 21:19:13 +0300

    Updates

--- assets/index-CAPffiRe.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DOkEUid9.js","assets/index-DqCjQ1xY.js","assets/index-BI-AcWuJ.css"])))=>i.map(i=>d[i]);
-import{r as p,_ as r}from"./index-DqCjQ1xY.js";const _=p("App",{web:()=>r(()=>import("./web-DOkEUid9.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
--- assets/index-CX6C1FIO.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-RKoKe414.js","assets/index-DqCjQ1xY.js","assets/index-BI-AcWuJ.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-DqCjQ1xY.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-RKoKe414.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
