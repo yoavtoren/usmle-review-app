4876e1109f3891cdbf723c0f93a56af242899ef1
Yoav Toren <yoavtoren@gmail.com>
2026-07-10 17:54:20 +0300

    Updates

--- assets/index-BAOsaEQV.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BMBtsRc7.js","assets/index-z6mMMqzf.js","assets/index-92pQEsUI.css"])))=>i.map(i=>d[i]);
-import{r as t,_ as n}from"./index-z6mMMqzf.js";var e;(function(r){r.Heavy="HEAVY",r.Medium="MEDIUM",r.Light="LIGHT"})(e||(e={}));var i;(function(r){r.Success="SUCCESS",r.Warning="WARNING",r.Error="ERROR"})(i||(i={}));const a=t("Haptics",{web:()=>n(()=>import("./web-BMBtsRc7.js"),__vite__mapDeps([0,1,2])).then(r=>new r.HapticsWeb)});export{a as Haptics,e as ImpactStyle,i as NotificationType};
---
+++ assets/web-D9CagM3B.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-Cbhn1K5x.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
