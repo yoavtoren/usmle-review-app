ba010d72992282ab912c186e03785db6e1fd6d0a
Yoav Toren <yoavtoren@gmail.com>
2026-07-10 17:04:45 +0300

    Updates

---
+++ assets/index-CN0xHRjr.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BqddZb-t.js","assets/index-DpFZtIbI.js","assets/index-2-CLuz22.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-DpFZtIbI.js";const _=p("App",{web:()=>r(()=>import("./web-BqddZb-t.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
